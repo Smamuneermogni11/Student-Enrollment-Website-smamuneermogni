b83e58661beb3877c42a7b710e1ac836ec0cb5c6
NaifAlblawi <112523960+NaifAlblawi@users.noreply.github.com>
2022-12-05 01:47:03 -0800

    test add course del and add room s

--- test.py
+++ test.py
@@ -58,11 +58,41 @@ def test_invalid_login():
     flask_app = create_app()
     with flask_app.test_client() as test_client:
         response = test_client.post('/login',
-                                    data=dict(email='naif.alblawi@admin.edu', password='1111'))
+                                    data=dict(name_loc='naif.alblawi@admin.edu', cap='1111'))
         response = test_client.get('/login')
         assert response.status_code == 200
 
+def test_saverloc():
+    flask_app = create_app()
+    with flask_app.test_client() as test_client:
+        response = test_client.post('/saverloc',
+                                    data=dict(name_loc='Test H 700', cap='60'))
+        response = test_client.get('/saverloc')
+        assert response.status_code == 200
 
+def test_del_loc():
+    flask_app = create_app()
+    with flask_app.test_client() as test_client:
+        response = test_client.post('/del_loc',
+                                    data=dict(locid='10'))
+        response = test_client.get('/del_loc')
+        assert response.status_code == 200
+
+def test_add_course():
+    flask_app = create_app()
+    with flask_app.test_client() as test_client:
+        response = test_client.post('/add_coursef',
+                                    data=dict(CourseCode='CSC 250', CourseName='Information Technology',CourseDescription='Information Technology',Department='3',Plan='1',Level='1',Credit='4',Day='1',From='8:00',To='12:00',Semester='222'))
+        response = test_client.get('/add_coursef')
+        assert response.status_code == 200
+
+def test_del_course():
+    flask_app = create_app()
+    with flask_app.test_client() as test_client:
+        response = test_client.post('/del_course',
+                                    data=dict(CourseID='200'))
+        response = test_client.get('/del_course')
+        assert response.status_code == 200
 
 def app():
     app = create_app()
